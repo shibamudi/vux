536dd288cae84d8d38c0e96919f9da6ff0ca5cda
shibamudi <919177684@qq.com>
2017-05-08 15:46:26 +0800

    增加 axios

--- meta.js
+++ meta.js
@@ -48,6 +48,10 @@ module.exports = {
       "type": "confirm",
       "message": "Install vuex?"
     },
+    "axios": {
+      "type": "confirm",
+      "message": "Install axios?"
+    },
     "lint": {
       "type": "confirm",
       "message": "Use ESLint to lint your code?"
@@ -91,7 +95,8 @@ module.exports = {
     "build/webpack.test.conf.js": "unit",
     "test/e2e/**/*": "e2e",
     "src/router/**/*": "router",
-    "src/store/**/*":　"vuex"
+    "src/store/**/*":　"vuex",
+    "src/api/**/*": "axios"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
---
+++ template/src/api/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import axios from 'axios'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+Vue.use(axios){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
+// API 请求放这里
+
+export default {}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
